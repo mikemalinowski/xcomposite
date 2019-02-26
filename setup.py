1cfd8a9cb873abf6099a93db53fbe8aa7639ed2f
mikemalinowski <mike.malinowski@outlook.com>
2019-02-26 16:31:29 +0000

    Bumping version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='xcomposite',
-    version='1.0.0',
+    version='1.0.1',
     author='Mike Malinowski',
     author_email='mike@twisted.space',
     description='A python package exposing the class composition design pattern',
